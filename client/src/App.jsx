bb03e08ea2445286e625c57d63a22c5368beb334
Anamika Rawat <anamikarawart@gmail.com>
2023-05-03 13:11:16 +0530

    client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-// import Chat from "@/components/chat";
-// import Login from "@/components/login";
+import Chat from "@/components/chat";
+import Login from "@/components/login";
 
 function App() {
 
-  // const [user, setUser] = useState(null);
-  // const [secret, setSecret] = useState(null);
-  // const isAuth = Boolean(user) && Boolean(secret);
+  const [user, setUser] = useState(null);
+  const [secret, setSecret] = useState(null);
+  const isAuth = Boolean(user) && Boolean(secret);
 
   return (
     <div className="app">
