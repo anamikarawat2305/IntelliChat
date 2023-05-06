3ab3bbe11a8ec7702eccd42c28687b1eb71296b3
Anamika Rawat <anamikarawart@gmail.com>
2023-05-06 14:52:08 +0530

    add server side

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+
 import { Configuration, OpenAIApi } from "openai";
+
+/* ROUTES */
 import openAiRoutes from "./routes/openai.js";
 import authRoutes from "./routes/auth.js";
 
